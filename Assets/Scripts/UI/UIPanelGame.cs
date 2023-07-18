3be32790e81a6982682da25883cd861dc396f889
Potato <minhtrong2k@gmail.com>
2023-07-18 18:12:17 +0700

    3. Create a restart button that when you click it, the game will play the previous level again.
    4. Edit the FillGapsWithNewItems function (of the Board class) to create a new item different from the 4 surrounding cells. At the same time, prioritize choosing the type of item that has the least amount on the board.

---
+++ Assets/Scripts/UI/UIPanelGame.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPanelGame : MonoBehaviour,IMenu
+{
+    public Text LevelConditionView;
+
+    [SerializeField] private Button btnPause;
+
+    [SerializeField] private Button btnReplayPreLevel;
+
+    private UIMainManager m_mngr;
+
+    private void Awake()
+    {
+        btnPause.onClick.AddListener(OnClickPause);
+        
+        btnReplayPreLevel.onClick.AddListener(this.OnClickReplayPreLevel);
+    }
+
+    private void OnClickPause()
+    {
+        m_mngr.ShowPauseMenu();
+    }
+
+    private void OnClickReplayPreLevel()
+    {
+        m_mngr.Reload();
+    }
+    
+    public void Setup(UIMainManager mngr)
+    {
+        m_mngr = mngr;
+    }
+
+    public void Show()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+}

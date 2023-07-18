65eaa2df53d5d8c3464cccf8677663965a002bab
Potato <minhtrong2k@gmail.com>
2023-07-18 16:16:50 +0700

    Reskin with scriptable objects

---
+++ Assets/Scripts/Board/ReskinData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ReskinData", menuName = "ScriptableObjects/ReskinData", order = 1)]
+public class ReskinData : ScriptableObject
+{
+    public Sprite sprite;
+}

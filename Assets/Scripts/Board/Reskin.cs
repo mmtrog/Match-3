45878814ab8e72ce87b22bf6640a810538d515b1
Potato <minhtrong2k@gmail.com>
2023-07-18 16:16:19 +0700

    Reskin with scriptable objects

---
+++ Assets/Scripts/Board/Reskin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class Reskin : MonoBehaviour
+{
+        public ReskinData data;
+
+        public SpriteRenderer spriteRenderer;
+        
+        private void Start()
+        {
+                if (this.data != null)
+                {
+                        this.spriteRenderer.sprite = this.data.sprite;
+                }          
+        }
+}
